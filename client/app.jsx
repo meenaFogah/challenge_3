9f92cb478708cdda9200a29eaa2972c064b2997d
meenaFogah <yasmeenfoqha2001@gmail.com>
2020-11-04 20:14:34 -0800

    before dimo

--- client/app.jsx
+++ client/app.jsx
@@ -1,32 +1,13 @@
-// app.use(express.static('public')
-import React from "react";
-function form1(props) {
+
+// const app()
+ function App(){
     return (
-      <div>
-        <p>Name: <input name="name" /></p>
-        <p>email: <input name="email" /></p>
-        <p>pass: <input name="pass" /></p>
-        {/*i have to chick the pass*/}
+      <div className="App">
+        <form>
+          <form component={form1} />
+          <form  component={form2} />
+        </form>
       </div>
     );
   }
-  
-  export default form1;
-
-// 
-import React from "react";
-
-function form2 (props) {
-  return (
-    <div>
-      <p>line1: <input name="line1" /></p>
-      <p>line2: <input name="line2" /></p>
-      <p>city: <input name="city" /></p>
-      <p>state: <input name="state" /></p>
-      <p>zip code: <input name="zip code" /></p>
-
-    </div>
-  );
-}
-
-export default form2 ;
+  export default App;
--- server.js
+++ server.js
@@ -1,2 +1,33 @@
+app.use(express.static('public')
 
+import React from "react";
+function form1(props) {
+    return (
+      <div>
+        <p>Name: <input name="name" /></p>
+        <p>email: <input name="email" /></p>
+        <p>pass: <input name="pass" /></p>
+        {/*i have to chick the pass*/}
+      </div>
+    );
+  }
+  
+  export default form1;
 
+// 
+import React from "react";
+
+function form2 (props) {
+  return (
+    <div>
+      <p>line1: <input name="line1" /></p>
+      <p>line2: <input name="line2" /></p>
+      <p>city: <input name="city" /></p>
+      <p>state: <input name="state" /></p>
+      <p>zip code: <input name="zip code" /></p>
+
+    </div>
+  );
+}
+
+export default form2 ;
