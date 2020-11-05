c6ccdc399392768945e80b05c00ac919e1f46d98
meenaFogah <yasmeenfoqha2001@gmail.com>
2020-11-04 16:03:10 -0800

    push 2

--- client/app.jsx
+++ client/app.jsx
@@ -1,2 +1,13 @@
-app.use(express.static('public')
+// app.use(express.static('public')
+import React from "react";
+function form1(props) {
+    return (
+      <div>
+        <p>Name: <input name="name" /></p>
+        <p>email: <input name="email" /></p>
+      </div>
+    );
+  }
+  
+  export default form1;
 
